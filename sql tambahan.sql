45bcb6bf9473e3970f73afaa38e2749c10d5bd4d
wiliamdecosta <wiliamdecosta@gmail.com>
2017-08-04 18:12:11 +0700

    Update sql tambahan.sql

--- sql tambahan.sql
+++ sql tambahan.sql
@@ -1,4 +1,4 @@
-CREATE TABLE permissions (
+CREATE TABLE sikp.permissions (
     permission_id integer NOT NULL,
     permission_name character varying(100) NOT NULL,
     permission_display_name character varying(255),
@@ -9,7 +9,7 @@ CREATE TABLE permissions (
     updated_date date
 );
 
-CREATE TABLE permission_role (
+CREATE TABLE sikp.permission_role (
     p_app_role_id integer,
     permission_id integer,
     created_by character varying(25),
@@ -18,7 +18,7 @@ CREATE TABLE permission_role (
     updated_date date
 );
 
-CREATE TABLE permission_menu (
+CREATE TABLE sikp.permission_menu (
     p_app_menu_id integer,
     permission_id integer,
     created_by character varying(25),
@@ -27,7 +27,7 @@ CREATE TABLE permission_menu (
     updated_date date
 );
 
-CREATE TABLE logs (
+CREATE TABLE sikp.logs (
     log_id integer NOT NULL,
     log_desc character varying(255) NOT NULL,
     log_user character varying(25),
@@ -37,27 +37,26 @@ CREATE TABLE logs (
     updated_date date
 );
 
-insert into permissions 
+insert into sikp.permissions 
 (permission_id, permission_name, permission_display_name, permission_description, created_by, created_date, updated_by, updated_date) values
 (1,	'can-view-user','','','admin','2017-05-29','admin',	'2017-05-29');
 
-insert into permission_role 
+insert into sikp.permission_role 
 (p_app_role_id, permission_id, created_by, creation_date, updated_by, updated_date) values
 (1,	1,	'admin',	'2017-05-29',	'admin',	'2017-05-29');
 
-ALTER TABLE p_application
+ALTER TABLE sikp.p_application
   ADD COLUMN module_icon character varying(100);
   
-ALTER TABLE p_application
+ALTER TABLE sikp.p_application
   ADD COLUMN is_active character varying(1);
 
-UPDATE p_application SET module_icon='images/md_01_on.png';
+UPDATE sikp.p_application SET module_icon='images/md_01_on.png';
 
-UPDATE p_application set is_active ='Y';
+UPDATE sikp.p_application set is_active ='Y';
 
-UPDATE p_app_user SET p_user_status_id =1 where p_app_user_id=1;
+UPDATE sikp.p_app_user SET p_user_status_id =1 where p_app_user_id=1;
 
-UPDATE "sikp"."p_app_menu" SET "file_name"='administration.p_application' WHERE ("p_app_menu_id"='7')
-UPDATE "sikp"."p_app_menu" SET "file_name"='administration.p_app_user' WHERE ("p_app_menu_id"='8')
-UPDATE "sikp"."p_app_menu" SET "file_name"='administration.p_app_role' WHERE ("p_app_menu_id"='6')
-
+UPDATE "sikp"."p_app_menu" SET "file_name"='administration.p_application' WHERE ("p_app_menu_id"='7');
+UPDATE "sikp"."p_app_menu" SET "file_name"='administration.p_app_user' WHERE ("p_app_menu_id"='8');
+UPDATE "sikp"."p_app_menu" SET "file_name"='administration.p_app_role' WHERE ("p_app_menu_id"='6');
