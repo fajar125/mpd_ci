2a00b76c556e9abb723b0a589efe7c3a7b44d1f3
saepudin06 <saepudin06@gmail.com>
2017-12-07 12:28:42 +0700

    ''

--- application/models/transaksi_wf/T_bphtb_registration_ro_ver.php
+++ application/models/transaksi_wf/T_bphtb_registration_ro_ver.php
@@ -81,7 +81,7 @@ class T_bphtb_registration_ro_ver extends Abstract_model {
                 object_rw='$object_rw', 
                 njop_pbb='$njop_pbb', 
                 object_address_name='$object_address_name', 
-                p_bphtb_legal_doc_type_id=$p_bphtb_legal_doc_type_id,
+                -- p_bphtb_legal_doc_type_id=$p_bphtb_legal_doc_type_id,
                 npop=$npop, 
                 npop_tkp=$npop_tkp, 
                 npop_kp=$npop_kp,
