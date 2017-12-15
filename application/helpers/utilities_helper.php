b805d5a4b7c1b49f621c532e85f93a806029fc8d
saepudin06 <saepudin06@gmail.com>
2017-12-15 12:34:34 +0700

    ''

--- application/helpers/utilities_helper.php
+++ application/helpers/utilities_helper.php
@@ -107,4 +107,22 @@ function compareDate($from_date, $to_date) {
     }
     return 1;
 }
+
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
 ?>
