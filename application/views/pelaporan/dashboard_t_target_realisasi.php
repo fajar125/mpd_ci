b9931f5f153dbcbd54a277f0fe6b0d6a445738df
saepudin06 <saepudin06@gmail.com>
2018-01-05 09:50:17 +0700

    ''

--- application/views/pelaporan/dashboard_t_target_realisasi.php
+++ application/views/pelaporan/dashboard_t_target_realisasi.php
@@ -18,49 +18,24 @@
         <script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
     </head>
 
-   <body class="page-header-fixed page-sidebar-closed-hide-logo page-content-white page-md page-header-fixed page-sidebar-fixed">
+   <body class="page-header-fixed page-sidebar-closed-hide-logo page-content-white page-md page-header-fixed page-sidebar-fixed" style="background-color: #64D76B !important;">
 <!-- breadcrumb -->
 <!-- end breadcrumb -->
 
 <div class="space-4"></div>
 <div class="row">
     <div class="col-xs-12">
-        <div class="tabbable">
-            <ul class="nav nav-tabs">
-                <li class="active">
-                    <a href="javascript:;" data-toggle="tab" aria-expanded="true" id="tab-1">
-                        <i class="blue"></i>
-                        <strong> Target VS Realisasi </strong>
-                    </a>
-                </li>
-                <li class="">
-                    <a href="javascript:;" data-toggle="tab" aria-expanded="true" id="tab-2">
-                        <i class="blue"></i>
-                        <strong> Per Bidang Pajak </strong>
-                    </a>
-                </li>
-                <li class="">
-                    <a href="javascript:;" data-toggle="tab" aria-expanded="true" id="tab-3">
-                        <i class="blue"></i>
-                        <strong> Per Jenis Pajak </strong>
-                    </a>
-                </li>
-                <li class="">
-                    <a href="javascript:;" data-toggle="tab" aria-expanded="true" id="tab-4">
-                        <i class="blue"></i>
-                        <strong> Bulanan Per Jenis Pajak </strong>
-                    </a>
-                </li>
-            </ul>
-        </div>
 
         <div class="tab-content no-border">
-            <div class="row">
+            <!-- <div class="row">
                 <div class="col-xs-12">
                    <table id="grid-table"></table>
                    <div id="grid-pager"></div>
                 </div>
-            </div>
+            </div> -->
+            <div style="text-align: center;"><img src="<?php echo base_url(); ?>assets/image/logo-2.png" width="50px" height="" alt=""></div>
+            <div style="text-align: center; color: #ffffff; font-weight: bold; font-size: 20px;">PEMERINTAH KABUPATEN LOMBOK UTARA</div>
+            <div style="text-align: center; color: #ffffff; font-weight: bold; font-size: 20px;">BADAN PENDAPATAN DAERAH</div>
             <hr>
             <div class="row">  
                 <div class="col-xs-6">
@@ -97,259 +72,27 @@
 </script>
 
 
+
 <script>
-$("#tab-2").on("click", function(event) {
 
-    event.stopPropagation();
-    var grid = $('#grid-table');
-    p_year_period_id = grid.jqGrid ('getGridParam', 'selrow');
-    code = grid.jqGrid ('getCell', p_year_period_id, 'year_code');
-
-    if(p_year_period_id == null) {
-        swal('Informasi','Silahkan pilih salah satu tahun target','info');
-        return false;
-    }
-
-    window.location.href = "<?php echo base_url();?>"+"dashboard_target_vs_realisasi/index?p_year_period_id="+p_year_period_id+"&code="+code+"&mode=bidang";
-
-});
-</script>
-<script>
-    jQuery(function($) {
-        var grid_selector = "#grid-table";
-        var pager_selector = "#grid-pager";
-
-        jQuery("#grid-table").jqGrid({
-            url: '<?php echo WS_JQGRID."pelaporan.t_target_realisasi_controller/read"; ?>',
-            datatype: "json",
-            mtype: "POST",
-            colModel: [
-                {label: 'ID Year', name: 'p_year_period_id', key: true, width: 5, sorttype: 'number', editable: true, hidden: true},
-                {label: 'Tahun',name: 'year_code',width: 75, align: "left",editable: true,
-                    editoptions: {
-                        size: 30,
-                        maxlength:32
-                    },
-                    editrules: {required: true}
-                },
-                {label: 'Target',name: 'target_amt',width: 150, align: "right",editable: true,
-                    formatter: 'number', formatoptions: { decimalPlaces: 2 },
-                    editoptions: {
-                        size: 30,
-                        maxlength:32
-                    },
-                    editrules: {required: true}
-                },
-                {label: 'Realisasi',name: 'realisasi_amt',width: 150, align: "right",editable: true,
-                    formatter: 'number', formatoptions: { decimalPlaces: 2 },
-                    editoptions: {
-                        size: 30,
-                        maxlength:32
-                    },
-                    editrules: {required: true}
-                },
-                {label: 'Persentase',name: 'persentase',width: 100, align: "right",editable: true,
-                    editoptions: {
-                        rows: 2,
-                        cols:50
-                    }
-                },
-                {label: 'Selisih',name: 'selisih',width: 150, align: "right",editable: true,
-                    formatter: 'number', formatoptions: { decimalPlaces: 2 },
-                    editoptions: {
-                        size: 30,
-                        maxlength:32
-                    },
-                    editrules: {required: true}
-                },
-                {label: 'Persentase Selisih',name: 'persen_selisih',width: 150, align: "right",editable: true,
-                    editoptions: {
-                        rows: 2,
-                        cols:50
-                    }
-                }
+    // untuk dapatkan tahun
+    var d = new Date();
+    var n = d.getFullYear();
 
 
-                
-            ],
-            height: '100%',
-            autowidth: true,
-            viewrecords: true,
-            rowNum: 10,
-            rowList: [10,20,50],
-            rownumbers: true, // show row numbers
-            rownumWidth: 35, // the width of the row numbers columns
-            altRows: true,
-            shrinkToFit: true,
-            multiboxonly: true,
-            onSelectRow: function (rowid) {
-                /*do something when selected*/
-                var celValue = $('#grid-table').jqGrid('getCell', rowid, 'p_year_period_id'); 
-                reloadChart(celValue);
-                reloadChart2(celValue);
+    // alert(n);
 
-            },
-            sortorder:'',
-            pager: '#grid-pager',
-            jsonReader: {
-                root: 'rows',
-                id: 'id',
-                repeatitems: false
-            },
-            loadComplete: function (response) {
-                if(response.success == false) {
-                    swal({title: 'Attention', text: response.message, html: true, type: "warning"});
-                }
+    reloadChart(n);
+    reloadChart2(n);
 
-                setTimeout(function(){
-                  $("#grid-table").setSelection($("#grid-table").getDataIDs()[0],true);
-                },500);
-
-            },
-            //memanggil controller jqgrid yang ada di controller read
-            editurl: '<?php echo WS_JQGRID."pelaporan.t_target_realisasi_controller/read"; ?>',
-            caption: "Daftar Target VS Realisasi"
-
-        });
-
-        jQuery('#grid-table').jqGrid('navGrid', '#grid-pager',
-            {   //navbar options
-                edit: false,
-                editicon: 'fa fa-pencil blue bigger-120',
-                add: false,
-                addicon: 'fa fa-plus-circle purple bigger-120',
-                del: false,
-                delicon: 'fa fa-trash-o red bigger-120',
-                search: true,
-                searchicon: 'fa fa-search orange bigger-120',
-                refresh: true,
-                afterRefresh: function () {
-                    // some code here
-                    jQuery("#detailsPlaceholder").hide();
-                },
-
-                refreshicon: 'fa fa-refresh green bigger-120',
-                view: false,
-                viewicon: 'fa fa-search-plus grey bigger-120'
-            },
-
-            {
-                // options for the Edit Dialog
-                closeAfterEdit: true,
-                closeOnEscape:true,
-                recreateForm: true,
-                serializeEditData: serializeJSON,
-                width: 'auto',
-                errorTextFormat: function (data) {
-                    return 'Error: ' + data.responseText
-                },
-                beforeShowForm: function (e, form) {
-                    var form = $(e[0]);
-                    style_edit_form(form);
-
-                },
-                afterShowForm: function(form) {
-                    form.closest('.ui-jqdialog').center();
-                },
-                afterSubmit:function(response,postdata) {
-                    var response = jQuery.parseJSON(response.responseText);
-                    if(response.success == false) {
-                        return [false,response.message,response.responseText];
-                    }
-                    return [true,"",response.responseText];
-                }
-            },
-            
-            {
-                //new record form
-                closeAfterAdd: false,
-                clearAfterAdd : true,
-                closeOnEscape:true,
-                recreateForm: true,
-                width: 'auto',
-                errorTextFormat: function (data) {
-                    return 'Error: ' + data.responseText
-                },
-                serializeEditData: serializeJSON,
-                viewPagerButtons: false,
-                beforeShowForm: function (e, form) {
-                    var form = $(e[0]);
-                    style_edit_form(form);
-                },
-                afterShowForm: function(form) {
-                    form.closest('.ui-jqdialog').center();
-                },
-                afterSubmit:function(response,postdata) {
-                    var response = jQuery.parseJSON(response.responseText);
-                    if(response.success == false) {
-                        return [false,response.message,response.responseText];
-                    }
-
-                    $(".tinfo").html('<div class="ui-state-success">' + response.message + '</div>');
-                    var tinfoel = $(".tinfo").show();
-                    tinfoel.delay(3000).fadeOut();
+    setTimeout(function() {
+        reloadChart(n);
+        reloadChart2(n);
+    },100000);
 
 
-                    return [true,"",response.responseText];
-                }
-            },
-            {
-                //delete record form
-                serializeDelData: serializeJSON,
-                recreateForm: true,
-                beforeShowForm: function (e) {
-                    var form = $(e[0]);
-                    style_delete_form(form);
-
-                },
-                afterShowForm: function(form) {
-                    form.closest('.ui-jqdialog').center();
-                },
-                onClick: function (e) {
-                    //alert(1);
-                },
-                afterSubmit:function(response,postdata) {
-                    var response = jQuery.parseJSON(response.responseText);
-                    if(response.success == false) {
-                        return [false,response.message,response.responseText];
-                    }
-                    return [true,"",response.responseText];
-                }
-            },
-            {
-                //search form
-                closeAfterSearch: false,
-                recreateForm: true,
-                afterShowSearch: function (e) {
-                    var form = $(e[0]);
-                    style_search_form(form);
-                    form.closest('.ui-jqdialog').center();
-                },
-                afterRedraw: function () {
-                    style_search_filters($(this));
-                }
-            },
-            {
-                //view record form
-                recreateForm: true,
-                beforeShowForm: function (e) {
-                    var form = $(e[0]);
-                }
-            }
-        );
-
-    });
-
-    function responsive_jqgrid(grid_selector, pager_selector) {
-
-        var parent_column = $(grid_selector).closest('[class*="col-"]');
-        $(grid_selector).jqGrid( 'setGridWidth', $(".page-content").width() );
-        $(pager_selector).jqGrid( 'setGridWidth', parent_column.width() );
-
-    }
-
-    function reloadChart(p_year_period_id) {
-        $.getJSON( "<?php echo base_url('Target_realisasi/target_realisasi_tahun?p_year_period_id=')?>"+p_year_period_id, function( items ) {
+    function reloadChart(year_code) {
+        $.getJSON( "<?php echo base_url('Target_realisasi_dash/target_realisasi_tahun?year_code=')?>"+year_code, function( items ) {
                 var target_amt = items[0][0];
                 var realisasi_amt = items[0][1];
                 var tahun = items[0][2];
@@ -395,8 +138,8 @@ $("#tab-2").on("click", function(event) {
         });
     }
 
-    function reloadChart2(p_year_period_id) {
-        $.getJSON( "<?php echo base_url('Target_realisasi/t_target_realisasi_bulan_all?p_year_period_id=')?>"+p_year_period_id, function( items ) {
+    function reloadChart2(year_code) {
+        $.getJSON( "<?php echo base_url('Target_realisasi_dash/t_target_realisasi_bulan_all?year_code=')?>"+year_code, function( items ) {
         //$.getJSON( "http://localhost/mpd/services/t_target_realisasi_bulan_all.php?p_year_period_id=28", function( items ) {
                 var target_amount = [];
                 var realisasi_amt = [];
