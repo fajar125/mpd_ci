e59e73f4af6d1ce19cc0f416800b6e734d011649
saepudin06 <saepudin06@gmail.com>
2018-01-05 20:26:28 +0700

    ''

--- application/views/transaksi/t_revenue_target_dtl.php
+++ application/views/transaksi/t_revenue_target_dtl.php
@@ -180,7 +180,7 @@ $("#tab-2").on("click", function(event) {
                             });
                         }
                     },
-                    formatter:'currency', formatoptions: {prefix:"", thousandsSeparator:'.'}
+                    formatter:'currency', formatoptions: {prefix:"", decimalSeparator:",", thousandsSeparator:'.'}
                 },
                 {label: 'Deskripsi',name: 'description',width: 200, align: "left",editable: true,
                     edittype:'textarea',
